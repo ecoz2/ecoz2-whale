8389d3501853d72ed4fdf90c4bf76b54d3e904b5
Carlos Rueda <carueda@mbari.org>
2020-04-13 20:01:39 -0700

    a few more variations

--- exerc01/variations.sh
+++ exerc01/variations.sh
@@ -1,23 +1,35 @@
 # Adjust these as convenient:
 N=16
 M=1024
-Aparam="-a=0.1"
+Aval=0.3
+Aparam="-a=${Aval}"
 Iparam=
 
+SECONDS=0
 echo "TRAINING..."
 for class in `ls data/sequences/TRAIN/M${M}/`; do
   ecoz2 hmm learn -N=${N} $Aparam $Iparam data/sequences/TRAIN/M${M}/${class} &
 done
 wait
+duration=$SECONDS
 echo
-echo "TRAINING COMPLETED.  N=$N  M=$M  Aparam=$Aparam  Iparam=$Iparam"
+echo "TRAINING COMPLETED. ($((duration / 60))mins:$((duration % 60))secs)"
+echo " N=$N  M=$M  Aparam=$Aparam  Iparam=$Iparam"
 
 echo
 echo "CLASSIFYING TRAINING SEQUENCES"
-ecoz2 hmm classify --models data/hmms/N${N}__M${M}_t3__a0.3 --sequences data/sequences/TRAIN/M${M}
+ecoz2 hmm classify --models data/hmms/N${N}__M${M}_t3__a${Aval} --sequences data/sequences/TRAIN/M${M}
 
 echo
 echo "CLASSIFYING TEST SEQUENCES"
-ecoz2 hmm classify --models data/hmms/N${N}__M${M}_t3__a0.3 --sequences data/sequences/TEST/M${M}
+ecoz2 hmm classify --models data/hmms/N${N}__M${M}_t3__a${Aval} --sequences data/sequences/TEST/M${M}
 
-echo "** DONE **"
+## the following as an exploration of those classes having at least 20 test instances:
+#echo
+#echo "CLASSIFYING A SUBSET OF THE TEST SEQUENCES"
+#ecoz2 hmm classify --models data/hmms/N${N}__M${M}_t3__a${Aval}/{A,Bm,C,D,E,F,G2,H,I,I2,I3,P}.hmm --sequences data/sequences/TEST/M${M}/{A,Bm,C,D,E,F,G2,H,I,I2,I3,P}
+
+duration=$SECONDS
+echo "** DONE **  ($((duration / 60))mins:$((duration % 60))secs)"
+echo " N=$N  M=$M  Aparam=$Aparam  Iparam=$Iparam"
+echo
