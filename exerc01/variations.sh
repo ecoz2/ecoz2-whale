7e6a3647726873eaf40988fb45df7dbbf54e5fdb
Carlos Rueda <carueda@mbari.org>
2020-05-04 11:12:32 -0700

    using parallel

--- exerc01/variations.sh
+++ exerc01/variations.sh
@@ -7,10 +7,17 @@ Iparam=
 
 SECONDS=0
 echo "TRAINING..."
-for class in `ls data/sequences/TRAIN/M${M}/`; do
-  ecoz2 hmm learn -N=${N} $Aparam $Iparam data/sequences/TRAIN/M${M}/${class} &
-done
-wait
+
+# In parallel, using all cores available:
+ls data/sequences/TRAIN/M${M} | \
+  parallel "ecoz2 hmm learn -N=${N} ${Aparam} ${Iparam} data/sequences/TRAIN/M${M}/{}"
+
+# If sequentially:
+#for class in `ls data/sequences/TRAIN/M${M}/`; do
+#  ecoz2 hmm learn -N=${N} $Aparam $Iparam data/sequences/TRAIN/M${M}/${class} &
+#done
+#wait
+
 duration=$SECONDS
 echo
 echo "TRAINING COMPLETED. ($((duration / 60))mins:$((duration % 60))secs)"
@@ -18,11 +25,15 @@ echo " N=$N  M=$M  Aparam=$Aparam  Iparam=$Iparam"
 
 echo
 echo "CLASSIFYING TRAINING SEQUENCES"
-ecoz2 hmm classify --models data/hmms/N${N}__M${M}_t3__a${Aval} --sequences data/sequences/TRAIN/M${M}
+cmd="ecoz2 hmm classify --models data/hmms/N${N}__M${M}_t3__a${Aval} --sequences data/sequences/TRAIN/M${M}"
+echo "$cmd"
+$cmd
 
 echo
 echo "CLASSIFYING TEST SEQUENCES"
-ecoz2 hmm classify --models data/hmms/N${N}__M${M}_t3__a${Aval} --sequences data/sequences/TEST/M${M}
+cmd="ecoz2 hmm classify --models data/hmms/N${N}__M${M}_t3__a${Aval} --sequences data/sequences/TEST/M${M}"
+echo "$cmd"
+$cmd
 
 ## the following as an exploration of those classes having at least 20 test instances:
 #echo
@@ -30,6 +41,7 @@ ecoz2 hmm classify --models data/hmms/N${N}__M${M}_t3__a${Aval} --sequences data
 #ecoz2 hmm classify --models data/hmms/N${N}__M${M}_t3__a${Aval}/{A,Bm,C,D,E,F,G2,H,I,I2,I3,P}.hmm --sequences data/sequences/TEST/M${M}/{A,Bm,C,D,E,F,G2,H,I,I2,I3,P}
 
 duration=$SECONDS
+echo
 echo "** DONE **  ($((duration / 60))mins:$((duration % 60))secs)"
 echo " N=$N  M=$M  Aparam=$Aparam  Iparam=$Iparam"
 echo
